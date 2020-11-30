6e3db2592fd50d5de3ad24b50f024c916ea8f227
Musienko Maksym <mmusiien@redhat.com>
2020-11-30 20:10:06 +0200

    uncoment trap and image var.

--- .ci/openshift_e2e.sh
+++ .ci/openshift_e2e.sh
@@ -18,7 +18,7 @@ set -u
 # print each command before executing it
 set -x
 
-#trap 'Catch_Finish $?' EXIT SIGINT
+trap 'Catch_Finish $?' EXIT SIGINT
 
 # Catch the finish of the job and write logs in artifacts.
 function Catch_Finish() {
@@ -43,7 +43,7 @@ export CI_COMPONENT="devworkspace-operator"
 # DEVWORKSPACE_OPERATOR env var exposed by Openshift CI in e2e test pod. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
 # Dependencies environment are defined here: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v5.yaml#L36-L38
 
-#export IMG=${DEVWORKSPACE_OPERATOR}
+export IMG=${DEVWORKSPACE_OPERATOR}
 
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
